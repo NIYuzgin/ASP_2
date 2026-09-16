60e71adf89bb2b1501f3f96cfbe83cc3d3d4d20d
Николай Немов <prost91@outlook.com>
2026-09-16 20:27:19 +0300

    Lesson 44 - TODO Scaffold Student

---
+++ Blazor/Academy/Models/Human.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+	public class Human
+	{
+
+		[Required]
+		[StringLength(50, MinimumLength =2)]
+		public string last_name { get; set; }
+		[RegularExpression("^[A-ZА-Я][a-zа-я]+$")]
+		
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		public string first_name { get; set; }
+
+		public string? middle_name { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		public DateOnly birth_date { get; set; }
+
+		[EmailAddress]
+		public string? email { get; set; }
+
+		[Phone]
+		public string? phone { get; set; }
+
+		public byte[]? photo { get; set; }
+
+	}
+}
---
+++ Blazor/Academy/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+	public class Student:Human
+	{
+		[Key]
+		public int stud_id { get; set; }
+
+		[Required]
+		public int group { get; set; }
+
+	}
+}
